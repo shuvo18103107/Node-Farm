f1e923643f5139fc5e5082e85f5f51ff9e451ee8
shuvo18103107 <18103107@iubat.edu>
2021-09-16 12:26:26 +0600

    ✅ Read and write files in async way

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-//reading and writing files
+//import file system module
 const fs = require('fs');
-//read a file 
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
-console.log(textIn);
-//write a file 
-const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
-fs.writeFileSync('./txt/output.txt', textOut);
-console.log('File Written!');
+//reading and writing files in sync way (blocking)
+// //read a file 
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+// console.log(textIn);
+// //write a file 
+// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync('./txt/output.txt', textOut);
+// console.log('File Written!');
+
+//Non-blocking async way
+fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+
+    if (err) return console.log('💥Error');
+    fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+        // console.log(data2);
+        fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+            // console.log(data3);
+            fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+                console.log('file written');
+            })
+        })
+    })
+})
