c0b256b7bad566291fc355e4e4522c9309d87128
shuvo18103107 <18103107@iubat.edu>
2021-09-17 12:44:08 +0600

    ♻️ routing

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //import file system module
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
 //................ Files..................
 //reading and writing files in sync way (blocking)
 // //read a file 
@@ -34,8 +35,35 @@ fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
 //createserver has a callback function which is fired of each time server has a request
 const server = http.createServer((req, res) => {
     // console.log(req);//get request object in terminal
+    console.log(req.url);
+    //routing has no relation in file like if we write in browser /any file name that will not open any file in browser for this we have need another special technique
+    //url module parse the parameter value into a nicely formatted object
+    //implementing routing 
+    const pathName = req.url;
+    if (pathName === '/' || pathName === '/overview') {
+        res.end('Hello from the Overview!')
+
+    }
+    else if (pathName === '/product') {
+        res.end('Hello from the Product Page!')
+
+    }
+    else {
+        //for showing statuscode, header on console
+        //http header is the piece of information that we are sending back as a response
+        //there are many types of standard header
+        //header and status code need declare bedore we send response
+        res.writeHead(404, {
+            'Content-type': 'text/html', //by this a browser now expect html
+            'my-own-header': 'hello'
+
+        })
+        res.end('<h1>Page not found!<h1>')
+        //network tab e we can see the details of header info by clicking the name
+    }
+
     //send back a response to the client 
-    res.end('Hello from the server!')
+    // res.end('Hello from the server!')
 })
 //listining incoming req from the client - starting up the server
 //port , localhost(local computer address), oprional callback func - whicj will be run as soon as server start
